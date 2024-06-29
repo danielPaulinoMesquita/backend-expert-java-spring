a351c595f6a79bcc30c0ee24c20d751a3d133938
Daniel Paulino <daniel-paulino@hotmail.com>
2024-06-29 19:54:22 -0300

    1- primeira configuração

---
+++ src/test/java/br/com/daniel/userserviceapi/UserServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daniel.userserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
