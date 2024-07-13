bf82eaa1ba0da0c734f3b0950003b725544c936c
Daniel Paulino <daniel-paulino@hotmail.com>
2024-07-13 01:20:17 -0300

    new: Added new exception, and ControllerAdvice

--- src/main/java/br/com/daniel/userserviceapi/service/UserService.java
+++ src/main/java/br/com/daniel/userserviceapi/service/UserService.java
@@ -2,6 +2,7 @@ package br.com.daniel.userserviceapi.service;
 
 import br.com.daniel.userserviceapi.mapper.UserMapper;
 import br.com.daniel.userserviceapi.repository.UserRepository;
+import br.com.userservice.commonslib.model.exceptions.ResourceNotFoundException;
 import br.com.userservice.commonslib.model.responses.UserResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,8 @@ public class UserService {
     private final UserMapper userMapper;
 
     public UserResponse findById(String id) {
-        return  userMapper.fromEntity(userRepository.findById(id).orElse(null));
+        return  userMapper.fromEntity(
+                userRepository.findById(id)
+                              .orElseThrow(() -> new ResourceNotFoundException("Object not Found. id" + id + ", Type: "+UserResponse.class.getSimpleName())));
     }
 }
